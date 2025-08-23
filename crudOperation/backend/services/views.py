b7b86c91f2978809d3dc4fb4329e98fd0feeb4ed
unknown <402308182@my.richfield.ac.za>
2025-08-23 23:35:25 +0200

    add serializer and viewSet for service model

--- crudOperation/backend/services/views.py
+++ crudOperation/backend/services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Services
+from .serializers import ServiceSerializer
 
 # Create your views here.
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Services.objects.all().order_by('-created_at')
+    serializer_class = ServiceSerializer
